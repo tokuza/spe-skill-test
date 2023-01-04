afe035f37d89804cb1b3ffb8652a07eae52d6c34
tokuza <66656238+tokuza@users.noreply.github.com>
2023-01-04 14:31:16 +0700

    Create narcissistic.php

---
+++ narcissistic.php
@@ -0,0 +1,17 @@
+<?php
+
+function narcissistic(int $number): bool {
+  $array   = str_split($number);
+  $nlenght = count($array);
+  $sum = 0;
+
+  foreach($array as $n){
+    $sum += pow((int)$n, $nlenght);
+  }	
+	
+  return $sum==$number;
+}
+echo narcissistic(1634);
+echo narcissistic(153);
+
+?>
